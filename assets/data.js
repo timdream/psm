6efbd193ac54c41fd96489440d8f1f27a0dac19f
Timothy Guan-tin Chien <timdream@gmail.com>
2020-06-07 19:03:20 -0700

    Clean up

--- assets/data.js
+++ assets/data.js
@@ -1,44 +1,6 @@
 var data = {
   "en": [
     {
-      "url": "http://www.bbc.com/news/world-asia-27184298",
-      "backgroundImageFileName": "ichef.bbci.co.uk-news-1024-media-images-74478000-jpg-_74478968_hi022054705.jpg",
-      "title": "Clashes as anti-nuclear protests hit Taiwan - BBC News",
-      "description": "Taiwanese police clash with anti-nuclear protesters demanding that the ruling Kuomintang Party stop construction of a fourth nuclear power plant.",
-      "siteTitle": "BBC News",
-      "iconImageFileName": "static.bbci.co.uk-news-1.147.0619-apple-touch-icon.png"
-    },
-    {
-      "url": "http://4am.tw/",
-      "backgroundImageFileName": "4am.tw-images-fb800.png",
-      "title": "Democracy at 4am : What unprecedented protest means for Taiwan.",
-      "description": "To defend the hard-earned democracy. We occupied the Legistlative Yuan of ROC, peacefully and rationally, Since 2014-03-19, tens of thousands citizens have joined us. Taiwan needs your attention and support. Today, we ask you to join us, in support of basic democratic values."
-    },
-    {
-      "url": "https://mayday.us/",
-      "backgroundImageFileName": "mayday.us-images-2016_mayday-logo-shield_square.png",
-      "title": "MAYDAY: End Big Money Politics",
-      "description": "We are a grassroots movement fighting to fix our broken political system. Building teams at the local level, we hold status quo politicians accountable at the ballot box. Help us elect candidates committed to fundamental reform!",
-      "siteTitle": "maydayus",
-      "iconImageFileName": "mayday.us-favicon.ico"
-    },
-    {
-      "url": "http://www.ted.com/talks/lawrence_lessig_we_the_people_and_the_republic_we_must_reclaim",
-      "backgroundImageFileName": "pi.tedcdn.com-r-pe.tedcdn.com-images-ted-28f3cfe3a001394ccfaafa3fd72b8e0d8be58613_1600x1200.jpg",
-      "title": "Lawrence Lessig: We the People, and the Republic we must reclaim",
-      "description": "There is a corruption at the heart of American politics, caused by the dependence of Congressional candidates on funding from the tiniest percentage of citizens. That's the argument at the core of this blistering talk by legal scholar Lawrence Lessig. With rapid-fire visuals, he shows how the funding process weakens the Republic in the most fundamental way, and issues a rallying bipartisan cry that will resonate with many in the U.S. and beyond.",
-      "siteTitle": "TED",
-      "iconImageFileName": "pa.tedcdn.com-favicon.svg"
-    },
-    {
-      "url": "https://www.eff.org/",
-      "backgroundImageFileName": "www.eff.org-files-2015-03-02-eff-og-3.png",
-      "title": "Electronic Frontier Foundation",
-      "description": "Defending your rights in the digital world",
-      "siteTitle": "Electronic Frontier Foundation",
-      "iconImageFileName": "www.eff.org-sites-all-themes-frontier-favicon.ico"
-    },
-    {
       "url": "https://taiwancanhelp.us/",
       "backgroundImageFileName": "taiwancanhelp-og.png",
       "title": "Taiwan Can Help",
@@ -48,12 +10,6 @@ var data = {
     }
   ],
   "zh-TW": [
-    {
-      "url": "http://nonukeyesvote.tw/whynonuke.php",
-      "backgroundImageFileName": "nonukeyesvote.tw-images-fbshare_v3.jpg",
-      "title": "認識：為什麼反核？ - 全國廢核行動平台",
-      "description": "福島核災後，越來越多台灣人驚覺核能可能帶來的風險與危害，進而重新思考零核電的可能。309 全台 20 萬公民上街表態，廢核已成為超過七成民眾的共識。你可能已經從許多媒體、網路上，片段地聽過各種反核的理由，這裡我們將整理幾個入門的重點摘要、推薦閱讀的書籍、影片與網站連結，讓你能更認識核電與能源的相關議題。"
-    },
     {
       "url": "http://g0v.tw/zh-TW/",
       "backgroundImageFileName": "g0v.tw-imgs-g0v-2line-white-s.jpg",
